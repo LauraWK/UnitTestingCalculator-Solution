29baa059d60e0f044e6f39b5fc7ef09ebd78e876
LauraWK <wilkenschildt10@gmail.com>
2018-02-08 11:37:36 +0100

    test

--- Calculator.Test.Unit/CalculatorUnitTests.cs
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -6,7 +6,6 @@ using NUnit.Framework;
 namespace Calculator.Test.Unit
 {
     [TestFixture]
-    [Author("Troels Jensen")]
     public class CalculatorUnitTests
     {
         private Calculator _uut;
@@ -80,6 +79,77 @@ namespace Calculator.Test.Unit
         {
             _uut.Add(a, b);
             Assert.That(_uut.Accumulator,Is.EqualTo(result));
+        }
+
+        [TestCase(4, 2, 2)]
+        [TestCase(5, 2, 3)]
+        [TestCase(12, 2, 10)]
+        public void Accumulator_Sub_ResultIsCorrect(double a, double b, double result)
+        {
+            _uut.Subtract(a,b);
+            Assert.That(_uut.Accumulator, Is.EqualTo(result));
+        }
+
+        [TestCase(4, 3, 12)]
+        [TestCase(2, 3, 6)]
+        [TestCase(5, 2, 10)]
+        public void Accumulator_Multi_ResultIsCorrect(double a, double b, double result)
+        {
+            _uut.Multiply(a, b);
+            Assert.That(_uut.Accumulator,Is.EqualTo(result));
+        }
+
+        [TestCase(5, 2, 25)]
+        [TestCase(4, 2, 16)]
+        [TestCase(3, 2, 9)]
+        public void Accumulator_Pow_ResultIsCorrect(double x, double exp, double result)
+        {
+            _uut.Power(x, exp);
+            Assert.That(_uut.Accumulator,Is.EqualTo(result));
+        }
+
+        [TestCase(10, 2, 5)]
+        [TestCase(4, 2, 2)]
+        [TestCase(20, 2, 10)]
+        public void Accumulator_Div_ResultIsCorrect(double a, double b, double result)
+        {
+            _uut.Divide(a, b);
+            Assert.That(_uut.Accumulator, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Accumulator_newAdd_resultIsCorrect()
+        {
+            _uut.Add(4, 5);
+            Assert.That(_uut.Add(6),Is.EqualTo(15));
+        }
+
+        [Test]
+        public void Accumulaor_newSubtract_resultIsCorrect()
+        {
+            _uut.Subtract(8, 3);
+            Assert.That(_uut.Subtract(2),Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Accumulator_newMultiply()
+        {
+            _uut.Multiply(3, 5);
+            Assert.That(_uut.Multiply(4),Is.EqualTo(60));
+        }
+
+        [Test]
+        public void Accumulator_NewRaiseNumbers()
+        {
+            _uut.Power(3, 2);
+            Assert.That(_uut.Power(3),Is.EqualTo(729));
+        }
+
+        [Test]
+        public void Accumulator_NewDivide()
+        {
+            _uut.Divide(20,5);
+            Assert.That(_uut.Divide(2),Is.EqualTo(2));
         }
     }
 }
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -11,9 +11,23 @@ namespace Calculator
             return Accumulator;
         }
 
+        public double Add(double addend)
+        {
+            double result = Accumulator + addend;
+            Accumulator = result;
+            return Accumulator;
+        }
+
         public double Subtract(double a, double b)
         {
             Accumulator= a - b;
+            return Accumulator;
+        }
+
+        public double Subtract(double subtractor)
+        {
+            double result = Accumulator - subtractor;
+            Accumulator = result;
             return Accumulator;
         }
 
@@ -23,15 +37,36 @@ namespace Calculator
             return Accumulator;
         }
 
+        public double Multiply(double multiplier)
+        {
+            double result = Accumulator * multiplier;
+            Accumulator = result;
+            return Accumulator;
+        }
+
         public double Power(double a, double b)
         {
             Accumulator = Math.Pow(a, b);
             return Accumulator;
         }
 
+        public double Power(double exponent)
+        {
+            double result = Math.Pow(Accumulator, exponent);
+            Accumulator = result;
+            return Accumulator;
+        }
+
         public double Divide(double a, double b)
         {
-            Accumulator= a / b;
+            Accumulator = a / b;
+            return Accumulator;
+        }
+
+        public double Divide(double divisor)
+        {
+            double result = Accumulator / divisor;
+            Accumulator = result;
             return Accumulator;
         }
 

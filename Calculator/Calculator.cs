1faa408a6dbf944cf79323ee2ee99820bfb090bc
LauraWK <wilkenschildt10@gmail.com>
2018-02-08 10:36:47 +0100

    Accumulator

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -4,24 +4,37 @@ namespace Calculator
 {
     public class Calculator
     {
+        public double Accumulator { get; private set; }
+        private double result;
         public double Add(double a, double b)
         {
-            return a + b;
+            result = a + b;
+            return result;
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            result= a - b;
+            return result;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            result = a * b;
+            return result;
         }
 
         public double Power(double a, double b)
         {
-            return Math.Pow(a, b);
+            result = Math.Pow(a, b);
+            return result;
         }
+
+        public double Accumulat()
+        {
+            return result;
+        }
+        
     }
+
 }

9828a152a35b0c0425524440c969a9cf93246bba
LauraWK <wilkenschildt10@gmail.com>
2018-02-08 10:45:33 +0100

    divide changes

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -37,7 +37,8 @@ namespace Calculator
 
         public double Divide(double a, double b)
         {
-            return a / b;
+            result= a / b;
+            return result;
         }
     }
 
